8083da7b978ccfe6d526a7db1e17960c391eb007
maksimovaei <elena.maksimova@rubius.com>
2018-11-23 12:37:23 +0700

    checpoint-starter

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/script.js',
+  'register-sw.js',
+  'styles.css',
+  'img/feather-sprite.svg',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
